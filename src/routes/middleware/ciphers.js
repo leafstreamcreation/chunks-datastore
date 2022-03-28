085dbee79cac15866cb3a724e9719b9566c798e8
Derek <shouldergiant@icloud.com>
2022-03-28 15:04:08 -0400

    remove extraneous json formatter

--- src/routes/middleware/ciphers.js
+++ src/routes/middleware/ciphers.js
@@ -3,38 +3,6 @@ const saltRounds = 13;
 
 const CryptoJS = require("crypto-js");
 
-const JsonFormatter = {
-  stringify: function(cipherParams) {
-    // create json object with ciphertext
-    var jsonObj = { ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64) };
-    // optionally add iv or salt
-    if (cipherParams.iv) {
-      jsonObj.iv = cipherParams.iv.toString();
-    }
-    if (cipherParams.salt) {
-      jsonObj.s = cipherParams.salt.toString();
-    }
-    // stringify json object
-    return JSON.stringify(jsonObj);
-  },
-  parse: function(jsonStr) {
-    // parse json string
-    var jsonObj = JSON.parse(jsonStr);
-    // extract ciphertext from json object, and create cipher params object
-    var cipherParams = CryptoJS.lib.CipherParams.create({
-      ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
-    });
-    // optionally extract iv or salt
-    if (jsonObj.iv) {
-      cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
-    }
-    if (jsonObj.s) {
-      cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
-    }
-    return cipherParams;
-  }
-};
-
 module.exports = (req, res, next) => {
   const credentials = async (name, password = "") => {
     const salt = await bcrypt.genSalt(saltRounds);
